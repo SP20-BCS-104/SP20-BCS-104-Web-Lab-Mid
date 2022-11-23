37d978bbe2a5f55027c6fc52c7ac9eaf77dc0580
SP20-BCS-104 <SP20-BCS-104@cuilahore.edu.pk>
2022-11-23 17:02:08 +0500

    first commit

---
+++ event.js
@@ -0,0 +1,9 @@
+$(function has(){
+
+    $("button").click(function(){
+    $("#q1").hide();
+     
+      }) 
+    })
+
+    // I have applied the "Hide" event on "download All football app" on div1
